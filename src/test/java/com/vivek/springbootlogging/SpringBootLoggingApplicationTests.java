80b2a4cb01d9bbd13557cb680dc5e052a445e124
Vivek Runwal <vivekrunwal7@gmail.com>
2022-07-07 00:43:54 +0530

    Spring boot logging

---
+++ src/test/java/com/vivek/springbootlogging/SpringBootLoggingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vivek.springbootlogging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLoggingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
